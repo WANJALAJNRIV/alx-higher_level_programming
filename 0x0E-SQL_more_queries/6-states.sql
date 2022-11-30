7febba7703eeca5ea842007f520015296b789791
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-11-30 13:41:43 +0000

    new files

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- creates a table 
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL, 
+	name VARCHAR(256) NOT NULL, 
+	PRIMARY KEY(id)); 
