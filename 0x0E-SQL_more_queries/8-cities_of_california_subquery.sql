68c4703e362baeaa5237e6e7d58cf9ec3a9ed484
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-11-30 14:07:09 +0000

    added database

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
-SELECT id, name 
-FROM cities 
-WHERE state_id = (SELECT state_id 
-			FROM states 
-			WHERE name = 'California') 
-ORDER BY cities.id ASC;
+SELECT $1.cities.id, $1.cities.name 
+FROM $1.cities 
+WHERE $1.states.state_id = (SELECT $1.states.state_id 
+			FROM $1.states 
+			WHERE $1.states.name = 'California') 
+ORDER BY $1.cities.id ASC;
