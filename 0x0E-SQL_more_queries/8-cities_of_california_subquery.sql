d2877a67a971ca84c3f553e4e3e5d9d2c6fd7483
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-11-30 14:09:49 +0000

    correction of database

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
-SELECT $1.cities.id, $1.cities.name 
+SELECT cities.id, cities.name 
 FROM $1.cities 
-WHERE $1.states.state_id = (SELECT $1.states.state_id 
+WHERE states.state_id = (SELECT states.state_id 
 			FROM $1.states 
-			WHERE $1.states.name = 'California') 
-ORDER BY $1.cities.id ASC;
+			WHERE states.name = 'California') 
+ORDER BY cities.id ASC;
