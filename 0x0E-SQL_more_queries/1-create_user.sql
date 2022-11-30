c0aaf843cbfefdfd5d325fb51e5630788a4385c8
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-11-30 13:54:02 +0000

    new file

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- creating a new useri
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
 GRANT  ALL PRIVILEGES ON *.*  TO 'user_0d_1'@'localhost';
 ~                                                                                                                                                                                                                                
 ~                                                                
