dddf198d81c44490e7761ef34208adcb6023213d
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-11-30 13:56:52 +0000

    file 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,6 @@
+-- creating a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_pwd';
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_pwd';
+
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localh
