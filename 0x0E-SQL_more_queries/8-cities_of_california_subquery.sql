497aa00bf8f03c609cd669f0eab3d0d5facede99
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-12-01 03:10:12 +0000

    task 8 done

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@ SELECT cities.id, cities.name
 FROM $1.cities 
 WHERE states.state_id = (SELECT states.state_id 
 			FROM $1.states 
-			WHERE states.name = 'California') 
+			WHERE states.name = `California`) 
 ORDER BY cities.id ASC;
