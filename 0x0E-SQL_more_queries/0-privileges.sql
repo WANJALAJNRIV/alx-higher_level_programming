fa56a7ce2092a1499dc582596ca45744d87a4984
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-12-01 02:57:24 +0000

    privilege -0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,4 @@
--- show provileges for user 1
-
+-- show provileges for user all users in local host 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- show privileges for user 2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
 ~                                           
