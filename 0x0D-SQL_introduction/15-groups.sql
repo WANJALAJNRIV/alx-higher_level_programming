96272ad311de1f3ce1cce4a7c65c2450eaeb6bb8
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-12-01 10:27:58 +0000

    removed values

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score in the table second_table
-SELECT second_table.score, COUNT(second_table.score) AS number FROM second_table GROUP BY second_table.score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
