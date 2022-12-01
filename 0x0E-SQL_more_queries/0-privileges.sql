95d1558765cbffa0bea2ea7b47099606c92684bc
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-12-01 03:00:39 +0000

    task done

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- show provileges for user all users in local host 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
-~                                           
+SHOW GRANTS FOR 'user_0d_2'@'localhost';   
