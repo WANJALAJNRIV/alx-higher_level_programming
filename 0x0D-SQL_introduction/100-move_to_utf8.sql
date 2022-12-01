cfd54b4eac2980267b96c5477f1ab5ebe82fcc60
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-12-01 10:31:19 +0000

    tasks

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
--- converts database, column and table  to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
-ALTER DATABASE hbtn_0c_0 CHARACTER SET =  utf8mb4 COLLATE  utf8mb4_unicode_ci;
+-- -- Converts the entire database hbtn_0c_0 to UTF8.
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- covert table
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
